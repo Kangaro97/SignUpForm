9471b5ea360ea20660bedb51ecd0ce38183e0fee
akela567 <akela567@inbox.ru>
2018-11-21 00:19:25 +0300

    Создан проект на vue

---
+++ vue-project/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="mdl-layout mdl-layout--fixed-header">
+    <!-- Header -->
+    <app-header></app-header>
+    <!-- Main part -->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Header from '@/views/Header.vue';
+
+export default {
+  components: {
+    AppHeader: Header
+  }
+}
+</script>
+
+
+<style>
+
+</style>
